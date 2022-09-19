81dd7f54e58340eff1ab372c4f9093993541b439
bcburin <cburin.bruno@gmail.com>
2022-09-19 18:22:18 -0300

    Added type hints to greeting services

--- compbot/services/greet_services.py
+++ compbot/services/greet_services.py
@@ -1,10 +1,10 @@
 
 
-def get_simple_greet_str(first_name):
+def get_simple_greet_str(first_name: str):
     greet_string = f'Oi, {first_name}!'
     return greet_string
 
 
-def get_complex_greet_str(full_name):
+def get_complex_greet_str(full_name: str):
     greet_str = f'Olá, {full_name}, tenha um ótimo dia!'
     return greet_str
