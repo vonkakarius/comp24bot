3f68dc8b845fb1e63c6906e539d95bbf510d7f95
bcburin <cburin.bruno@gmail.com>
2022-09-28 23:36:59 -0300

    Use argparse to parse CLI arguments

--- compbot/main.py
+++ compbot/main.py
@@ -1,26 +1,31 @@
 import logging
-from sys import argv
+from argparse import ArgumentParser
+from os import environ
+
 from telegram.ext import ApplicationBuilder
 
 from compbot.api import add_api
 
 
-def get_token() -> str:
-    if len(argv) < 2:
-        raise ValueError('Provide bot token as a command line argument')
-    return argv[1]
-
-
 if __name__ == '__main__':
+    # Add argument parser
+    parser = ArgumentParser(description='Get configuration and sensitive data from server')
+    parser.add_argument('token', help='token used to control bot')
+    parser.add_argument(
+        '-p', '--production',
+        help='set if process is run in production server',
+        action='store_true'
+    )
+    args = parser.parse_args()
+    # Setup logger
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
     )
-    token = get_token()
     # Build application
-    application = ApplicationBuilder().token(token).build()
+    application = ApplicationBuilder().token(args.token).build()
     # Add command API
-    add_api(application=application)
+    add_api(application=application, in_prod=args.production)
     # Start server application
     application.run_polling()
 
